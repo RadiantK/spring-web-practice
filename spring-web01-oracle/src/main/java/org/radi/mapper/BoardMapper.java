76ed536afbc67b62863c08e8a4ead2c52516bbd7
RadiantK <pps8853@gmail.com>
2022-07-10 21:37:35 +0900

    Mapper 인터페이스에 페이징 처리 추가 및 목록 총게시글 수를 얻을 메소드 추가
    Mapper 인터페이스에 페이징 처리 추가 및 목록 총게시글 수를 얻을 메소드 추가

--- spring-web01-oracle/src/main/java/org/radi/mapper/BoardMapper.java
+++ spring-web01-oracle/src/main/java/org/radi/mapper/BoardMapper.java
@@ -3,11 +3,16 @@ package org.radi.mapper;
 import java.util.List;
 
 import org.radi.domain.BoardVO;
+import org.radi.domain.Criteria;
 
 public interface BoardMapper {
 
 	// @Select("SELECT * FROM tbl_board WHERE bno > 0")
 	public List<BoardVO> getList();
+	
+	public List<BoardVO> getListWithPaging(Criteria cri);
+	
+	public int getTotalCount(Criteria cri);
 	// insert만 처리되고 pk는 알 필요가 없는경우
 	public void insert(BoardVO board);
 	// insert가 처리되고 pk를 알 필요가 있는경우
@@ -18,4 +23,5 @@ public interface BoardMapper {
 	public int delete(Long bno);
 	
 	public int update(BoardVO board);
+	
 }
