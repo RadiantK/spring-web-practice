ed1969387ec3bf7cde600c3dedf332e87296e4a5
RadiantK <pps8853@gmail.com>
2022-07-10 21:35:02 +0900

    페이지 관련 정보를 담을 DTO
    페이지 관련 정보를 담을 DTO

---
+++ spring-web01-oracle/src/main/java/org/radi/domain/PageDTO.java
@@ -0,0 +1,34 @@
+package org.radi.domain;
+
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+public class PageDTO {
+
+	private int startPage;
+	private int endPage;
+	private boolean prev, next;
+	
+	private int total;
+	private Criteria cri;
+	
+	public PageDTO(Criteria cri, int total) {
+		
+		this.cri = cri;
+		this.total = total;
+		
+		// 끝페이지 번호
+		this.endPage = (int)(Math.ceil(cri.getPageNum() / 10.0)) * 10;
+		// 시작페이지 번호
+		this.startPage = this.endPage - 9;
+		// 페이지의 마지막 번호
+		int realEnd = (int)(Math.ceil((total * 1.0) / cri.getAmount()));
+		
+		if(endPage > realEnd) endPage = realEnd;
+		
+		this.prev = this.startPage > 1;
+		this.next = this.endPage < realEnd;
+	}
+}
