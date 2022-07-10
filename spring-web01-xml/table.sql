0accdb6aa6ba033616f2f4a0aefcd2feb74570e4
RadiantK <pps8853@gmail.com>
2022-07-10 11:18:25 +0900

    테이블 수정
    테이블 수정

--- spring-web01-xml/table.sql
+++ spring-web01-xml/table.sql
@@ -1,3 +1,4 @@
+-- mysql용
 CREATE TABLE  tbl_board (
 	bno INT(10),
     title VARCHAR(200) NOT NULL,
@@ -10,3 +11,20 @@ CREATE TABLE  tbl_board (
 ALTER TABLE tbl_board add constraint pk_board primary key (bno);
 
 alter table tbl_board MODIFY bno INT(10) NOT NULL auto_increment;
+
+
+-- 오라클용
+
+CREATE TABLE  tbl_board (
+	bno NUMBER(10),
+    title VARCHAR2(200) NOT NULL,
+    content VARCHAR2(2000) not null,
+    writer varchar2(50) not null,
+    regdate DATE default sysdate,
+    updatedate DATE default sysdate
+);
+
+CREATE SEQUENCE seq_board;
+
+ALTER table tbl_board add constraint pk_board primary key (bno);
+ALTER table tbl_board modify bno number(10) NOT NULL;
