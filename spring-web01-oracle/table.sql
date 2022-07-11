491e1bebaa3e5f39620415d5f27b345a402ab6e6
RadiantK <pps8853@gmail.com>
2022-07-11 23:24:37 +0900

    Update table.sql

--- spring-web01-oracle/table.sql
+++ spring-web01-oracle/table.sql
@@ -1,19 +1,7 @@
--- mysql용
-CREATE TABLE  tbl_board (
-	bno INT(10),
-    title VARCHAR(200) NOT NULL,
-    content VARCHAR(2000) not null,
-    writer varchar(50) not null,
-    regdate DATETIME default now(),
-    updatedate DATETIME default now()
-);
-
-ALTER TABLE tbl_board add constraint pk_board primary key (bno);
-
-alter table tbl_board MODIFY bno INT(10) NOT NULL auto_increment;
-
-
 -- 오라클용
+
+drop table tbl_board;
+drop sequence seq_board;
 
 CREATE TABLE  tbl_board (
 	bno NUMBER(10),
