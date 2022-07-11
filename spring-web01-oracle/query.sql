ecadc18a0bfa0961000df626fd5634cd1544ab74
RadiantK <pps8853@gmail.com>
2022-07-11 23:28:12 +0900

    프로젝트에 사용된 쿼리
    프로젝트에 사용된 쿼리

--- spring-web01-oracle/query.sql
+++ spring-web01-oracle/query.sql
@@ -14,4 +14,34 @@ rownum rn, bno, title, content from tbl_board where bno > 0;
 select /*+ INDEX_DESC(tbl_board pk_board) */
 rownum rn, bno, title, content from tbl_board where bno > 0;
 
-SELECT bno, title, content
+
+
+SELECT bno, title, content, writer, regdate, updatedate
+FROM (
+    SELECT /*+ INDEX_DESC(tbl_board pk_board)*/
+        ROWNUM rn, bno, title, content, writer, regdate, updatedate
+    FROM tbl_board
+    WHERE ROWNUM <= 30 )
+WHERE RN > 20;
+
+
+-- 페이징 1
+SELECT bno, title, content, writer, regdate, updatedate
+FROM (
+    SELECT /*+ INDEX_DESC(tbl_board pk_board) */
+        ROWNUM rn, bno, title, content, writer, regdate, updatedate
+    FROM tbl_board
+    WHERE
+        (title LIKE '%테스트%' OR content LIKE '%테스트%')
+        AND ROWNUM <= 20 )
+WHERE rn > 10;
+
+-- 페이징 2
+SELECT bno, title, content, writer, regdate, updatedate
+FROM (
+    SELECT /*+ INDEX_DESC(tbl_board pk_board) */
+        ROWNUM rn, bno, title, content, writer, regdate, updatedate
+    FROM tbl_board
+    WHERE
+        title LIKE '%테스트%' OR content LIKE '%테스트%')
+WHERE rn > 10 AND rn <= 20;
