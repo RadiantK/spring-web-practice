d193627d91f7261fff624fb03d6df2837e569c5b
RadiantK <pps8853@gmail.com>
2022-07-11 23:23:45 +0900

    queryParameter를 UriComponentBuilder를 사용해서 생성
    queryParameter를 UriComponentBuilder를 사용해서 생성

--- spring-web01-oracle/src/main/java/org/radi/controller/BoardController.java
+++ spring-web01-oracle/src/main/java/org/radi/controller/BoardController.java
@@ -74,10 +74,12 @@ public class BoardController {
 			rttr.addFlashAttribute("result", "success");
 		}
 		
-		rttr.addAttribute("pageNum", cri.getPageNum());
-		rttr.addAttribute("amount", cri.getAmount());
+//		rttr.addAttribute("pageNum", cri.getPageNum());
+//		rttr.addAttribute("amount", cri.getAmount());
+//		rttr.addAttribute("type", cri.getType());
+//		rttr.addAttribute("keyword", cri.getKeyword());
 		
-		return "redirect:/board/list";
+		return "redirect:/board/list" + cri.getListLink();
 	}
 	
 	@PostMapping("/remove")
@@ -89,9 +91,11 @@ public class BoardController {
 			rttr.addFlashAttribute("result", "success");
 		}
 		
-		rttr.addAttribute("pageNum", cri.getPageNum());
-		rttr.addAttribute("amount", cri.getAmount());
+//		rttr.addAttribute("pageNum", cri.getPageNum());
+//		rttr.addAttribute("amount", cri.getAmount());
+//		rttr.addAttribute("type", cri.getType());
+//		rttr.addAttribute("keyword", cri.getKeyword());
 		
-		return "redirect:/board/list";
+		return "redirect:/board/list" + cri.getListLink();
 	}
 }
--- spring-web01-oracle/src/main/java/org/radi/domain/Criteria.java
+++ spring-web01-oracle/src/main/java/org/radi/domain/Criteria.java
@@ -1,5 +1,7 @@
 package org.radi.domain;
 
+import org.springframework.web.util.UriComponentsBuilder;
+
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -9,8 +11,11 @@ import lombok.ToString;
 @ToString
 public class Criteria {
 
-	private int pageNum;
-	private int amount;
+	private int pageNum; // 페이지 번호
+	private int amount; // 한페이지당 출력할 게시글 수
+	
+	private String type; // 검색어 종류
+	private String keyword; // 키워드
 	
 	public Criteria() {
 		this(1, 10);
@@ -21,4 +26,20 @@ public class Criteria {
 		this.amount = amount;
 	}
 	
+	public String[] getTypeArr() {
+		
+		return type == null ? new String[] {} : type.split("");
+	}
+	
+	public String getListLink() {
+		
+		// 여러 개의 파라미터들을 연결해서 URL 형태로 만들어줌
+		UriComponentsBuilder builder = UriComponentsBuilder.fromPath("")
+				.queryParam("pageNum", this.pageNum)
+				.queryParam("amount", this.getAmount())
+				.queryParam("amount", this.getType())
+				.queryParam("amount", this.getKeyword());
+		
+		return builder.toUriString();
+	}
 }
