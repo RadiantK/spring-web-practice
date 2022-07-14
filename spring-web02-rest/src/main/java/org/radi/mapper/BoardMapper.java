5b80a5294a40c5403d8eaaf0ba030c94a8351f70
RadiantK <pps8853@gmail.com>
2022-07-14 23:34:37 +0900

    spring-web01 프로젝트의 게시판  복사
    spring-web01 프로젝트의 게시판  복사

---
+++ spring-web02-rest/src/main/java/org/radi/mapper/BoardMapper.java
@@ -0,0 +1,27 @@
+package org.radi.mapper;
+
+import java.util.List;
+
+import org.radi.domain.BoardVO;
+import org.radi.domain.Criteria;
+
+public interface BoardMapper {
+
+	// @Select("SELECT * FROM tbl_board WHERE bno > 0")
+	public List<BoardVO> getList();
+	
+	public List<BoardVO> getListWithPaging(Criteria cri);
+	
+	public int getTotalCount(Criteria cri);
+	// insert만 처리되고 pk는 알 필요가 없는경우
+	public void insert(BoardVO board);
+	// insert가 처리되고 pk를 알 필요가 있는경우
+	public void insertSelectKey(BoardVO board);
+	
+	public BoardVO read(Long bno);
+	
+	public int delete(Long bno);
+	
+	public int update(BoardVO board);
+	
+}
