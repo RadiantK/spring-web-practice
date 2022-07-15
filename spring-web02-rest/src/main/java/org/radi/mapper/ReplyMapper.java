af4ba465207b25cc289b49fcf8a30533bfadf4ea
RadiantK <pps8853@gmail.com>
2022-07-15 17:57:10 +0900

    댓글 관련 mybatis mapper파일 생성
    댓글 관련 mybatis mapper xml, interface파일을 생성하고 CRUD 구현

---
+++ spring-web02-rest/src/main/java/org/radi/mapper/ReplyMapper.java
@@ -0,0 +1,22 @@
+package org.radi.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+import org.radi.domain.Criteria;
+import org.radi.domain.ReplyVO;
+
+public interface ReplyMapper {
+
+	List<ReplyVO> getListWithPaging(
+			@Param("cri") Criteria cri,
+			@Param("bno") Long bno);
+	
+	ReplyVO read(long rno); // 특정 댓글 읽기
+	
+	int insert(ReplyVO vo);
+	
+	int delete(Long rno);
+	
+	int update(ReplyVO reply);
+}
