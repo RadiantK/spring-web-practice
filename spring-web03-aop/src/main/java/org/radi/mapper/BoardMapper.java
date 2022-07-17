4e25ff8152d02586309166d0e74cd9c29fd23398
RadiantK <pps8853@gmail.com>
2022-07-17 13:36:35 +0900

    boardMapper replyCnt처리용 SQL 추가
    boardMapper replyCnt처리용 SQL 추가

--- spring-web03-aop/src/main/java/org/radi/mapper/BoardMapper.java
+++ spring-web03-aop/src/main/java/org/radi/mapper/BoardMapper.java
@@ -2,6 +2,7 @@ package org.radi.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
 import org.radi.domain.BoardVO;
 import org.radi.domain.Criteria;
 
@@ -23,5 +24,11 @@ public interface BoardMapper {
 	public int delete(Long bno);
 	
 	public int update(BoardVO board);
+	
+	// amount : 증가나 감소를 의미 (댓글증가 +1, 댓글감소 -1)
+	// MyBatis는 SQL을 처리할 때 하나의 파라미터 타입을 사용하기 때문에 2개 이상의
+	// 데이터를 전달하려면 @Param 어노테이션을 사용해서 처리 가능
+	public void updateReplyCnt(
+			@Param("bno") Long bno, @Param("amount") int amount);
 	
 }
