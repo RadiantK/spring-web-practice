c858636ee2efd3758b781f1863249ddb29bc30c3
RadiantK <pps8853@gmail.com>
2022-07-17 12:39:36 +0900

    프로젝트 게시판 및 댓글 처리 세팅
    프로젝트 게시판 및 댓글 처리 세팅

---
+++ spring-web03-aop/query.sql
@@ -0,0 +1,62 @@
+-- tbl_board
+INSERT INTO tbl_board(bno, title, content, writer) 
+VALUES(seq_board.nextval, '테스트 제목', '테스트 내용', 'user00');
+commit;
+
+-- 테이블 재귀 호출
+INSERT INTO tbl_board(bno, title,content, writer)
+(SELECT seq_board.nextval, title, content, writer from tbl_board);
+
+
+-- 오라클 힌트 사용
+select /*+ FULL(tbl_board) */
+rownum rn, bno, title, content from tbl_board where bno > 0;
+
+select /*+ INDEX_DESC(tbl_board pk_board) */
+rownum rn, bno, title, content from tbl_board where bno > 0;
+
+
+
+SELECT bno, title, content, writer, regdate, updatedate
+FROM (
+    SELECT /*+ INDEX_DESC(tbl_board pk_board)*/
+        ROWNUM rn, bno, title, content, writer, regdate, updatedate
+    FROM tbl_board
+    WHERE ROWNUM <= 30 )
+WHERE RN > 20;
+
+
+-- 페이징 1
+SELECT bno, title, content, writer, regdate, updatedate
+FROM (
+    SELECT /*+ INDEX_DESC(tbl_board pk_board) */
+        ROWNUM rn, bno, title, content, writer, regdate, updatedate
+    FROM tbl_board
+    WHERE
+        (title LIKE '%테스트%' OR content LIKE '%테스트%')
+        AND ROWNUM <= 20 )
+WHERE rn > 10;
+
+-- 페이징 2
+SELECT bno, title, content, writer, regdate, updatedate
+FROM (
+    SELECT /*+ INDEX_DESC(tbl_board pk_board) */
+        ROWNUM rn, bno, title, content, writer, regdate, updatedate
+    FROM tbl_board
+    WHERE
+        title LIKE '%테스트%' OR content LIKE '%테스트%')
+WHERE rn > 10 AND rn <= 20;
+
+
+
+-- tbl_reply
+
+-- 페이징
+SELECT rno, bno, reply, replyer, replydate, updatedate
+FROM (
+    SELECT /*+ INDEX(tbl_reply idx_reply */
+        ROWNUM rn, bno, rno, reply, replyer, replydate, updatedate
+    FROM tbl_reply
+    WHERE bno = 144 AND rno > 0
+    )
+WHERE rn > 5 AND rn <= 15;
---
+++ spring-web03-aop/src/main/java/org/radi/controller/BoardController.java
@@ -0,0 +1,101 @@
+package org.radi.controller;
+
+import org.radi.domain.BoardVO;
+import org.radi.domain.Criteria;
+import org.radi.domain.PageDTO;
+import org.radi.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j;
+
+@Controller
+@Log4j
+@RequestMapping("/board/*")
+@AllArgsConstructor
+public class BoardController {
+
+	@Autowired
+	private BoardService service;
+	
+	@GetMapping("/list")
+	public void list(Criteria cri,Model model) {
+		
+		log.info("list: " + cri);
+		
+		model.addAttribute("list", service.getList(cri));
+		//model.addAttribute("pageMaker", new PageDTO(cri, 123));
+		
+		int total = service.getTotal(cri);
+		
+		log.info("total: " + total);
+		
+		model.addAttribute("pageMaker", new PageDTO(cri, total));
+	}
+	
+	@GetMapping("/register")
+	public void register() {
+		
+	}
+	
+	@PostMapping("/register")
+	public String register(BoardVO board, RedirectAttributes rttr) {
+		
+		log.info("register: " + board);
+		service.register(board);
+		// 화면에 한 번만 사용하고 다음에는 사용되지 않는 데이터를 전달하기 위해서 사용
+		rttr.addFlashAttribute("result", board.getBno());
+		
+		return "redirect:/board/list";
+	}
+	
+	@GetMapping({"/get", "/modify"})
+	public void get(@RequestParam("bno") Long bno, 
+			@ModelAttribute("cri") Criteria cri,Model model) {
+		
+		log.info("/get or modify");
+		model.addAttribute("board", service.get(bno));
+	}
+	
+	@PostMapping("/modify")
+	public String modify(BoardVO board,
+			@ModelAttribute("cri") Criteria cri ,RedirectAttributes rttr) {
+		log.info("modify: " + board);
+		
+		if(service.modify(board)) {
+			rttr.addFlashAttribute("result", "success");
+		}
+		
+//		rttr.addAttribute("pageNum", cri.getPageNum());
+//		rttr.addAttribute("amount", cri.getAmount());
+//		rttr.addAttribute("type", cri.getType());
+//		rttr.addAttribute("keyword", cri.getKeyword());
+		
+		return "redirect:/board/list" + cri.getListLink();
+	}
+	
+	@PostMapping("/remove")
+	public String remove(@RequestParam("bno") Long bno,
+			@ModelAttribute("cri") Criteria cri,RedirectAttributes rttr) {
+		log.info("remove....." + bno);
+		
+		if(service.remove(bno)) {
+			rttr.addFlashAttribute("result", "success");
+		}
+		
+//		rttr.addAttribute("pageNum", cri.getPageNum());
+//		rttr.addAttribute("amount", cri.getAmount());
+//		rttr.addAttribute("type", cri.getType());
+//		rttr.addAttribute("keyword", cri.getKeyword());
+		
+		return "redirect:/board/list" + cri.getListLink();
+	}
+}
