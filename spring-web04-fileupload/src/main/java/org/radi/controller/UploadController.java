ddba9caed8d5392bc5d280fd97ac3bd45d9f9ff0
RadiantK <pps8853@gmail.com>
2022-07-19 00:55:29 +0900

    프로젝트 파일 다운로드 및 제거
    프로젝트 파일 다운로드 및 제거

--- spring-web04-fileupload/src/main/java/org/radi/controller/UploadController.java
+++ spring-web04-fileupload/src/main/java/org/radi/controller/UploadController.java
@@ -3,6 +3,9 @@ package org.radi.controller;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
 import java.nio.file.Files;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -11,6 +14,8 @@ import java.util.List;
 import java.util.UUID;
 
 import org.radi.domain.AttachFileDTO;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -20,6 +25,7 @@ import org.springframework.ui.Model;
 import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -136,6 +142,7 @@ public class UploadController {
 		return new ResponseEntity<>(list, HttpStatus.OK);
 	}
 	
+	// 파일 읽어오기(섬네일 출력)
 	@ResponseBody
 	@GetMapping("/display")
 	public ResponseEntity<byte[]> getFile(String fileName) {
@@ -159,6 +166,95 @@ public class UploadController {
 			e.printStackTrace();
 		}
 		return result;
+	}
+	
+	//첨부파일 다운로드 MediaType.APPLICATION_OCTET_STREAM_VALUE 8비트 바이너리 데이터를 위한 스트림
+	@ResponseBody
+	@GetMapping(value = "/download", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+	public ResponseEntity<Resource> downloadFile(
+			@RequestHeader("User-Agent") String userAgent, // 헤더 정보 읽어오기
+			String fileName) {
+		
+		log.info("download file: " + fileName);
+		// Resource : 자원에 대한 접근을 추상화하기 위한 인터페이스
+		Resource resource = new FileSystemResource("d:\\upload\\" + fileName);
+		
+		if(resource.exists() == false) { // 파일이 없으면
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		
+		log.info("resource: " + resource);
+		
+		String resourceName = resource.getFilename(); // 해당 자원의 파일이름
+		
+		// remove UUID
+		String resourceOriginalName = 
+				resourceName.substring(resourceName.indexOf("_")+1);
+		
+		HttpHeaders headers = new HttpHeaders();
+		
+		try {
+			String downloadName = null;
+			
+			if(userAgent.contains("Trident")) { // IE엔진이름
+				log.info("IE browser");
+				
+				downloadName = URLEncoder.encode(resourceOriginalName, "UTF-8")
+						.replaceAll("\\+", " "); // +문자 공백으로 변환
+				
+				log.info("Edge name: " + downloadName);
+			}else if(userAgent.contains("Edge")){
+				log.info("Edge browser");
+				
+				downloadName = URLEncoder.encode(resourceOriginalName, "UTF-8");
+			}else {
+				
+				log.info("Chrome browser");
+				// 디코딩
+				downloadName = new String(resourceOriginalName.getBytes("UTF-8"), "ISO-8859-1");
+			}
+			
+			// content-Disposition은 HTTP ResponseBody에 오는 컨텐츠의 성향을 알려주는 속성
+			// 기본값은 inline으로 web에 전달되는 data
+			// attachment; fileName= 값으로 주면 body에 오는 값을 다운로드 받으라는 뜻이 됨
+			headers.add("Content-Disposition", "attachment; fileName=" + 
+					downloadName);
+		} catch(UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+		return new ResponseEntity<Resource>(resource, headers, HttpStatus.OK);
+	}
+	
+	//파일 삭제
+	@ResponseBody
+	@PostMapping("/deleteFile")
+	public ResponseEntity<String> deleteFile(String fileName, String type) {
+		
+		log.info("deleteFile: " + fileName);
+		
+		File file;
+		
+		try {
+			file = new File("d\\upload\\" + URLDecoder.decode(fileName, "UTF-8"));
+			
+			file.delete();
+			
+			if(type.equals("image")) {
+				
+				// 섬네일 제거
+				String largeFileName = file.getAbsolutePath().replace("s_", "");
+				
+				log.info("largeFileName: " + largeFileName);
+				
+				file = new File(largeFileName);
+				
+				file.delete();
+			}
+		}catch(UnsupportedEncodingException e) {
+			e.printStackTrace();
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<>("deleted", HttpStatus.OK);
 	}
 	
 	// 현재 날짜의 폴더 생성
