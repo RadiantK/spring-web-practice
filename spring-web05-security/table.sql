2d1a64042f7c93fe4181b07d168a884d76e3ef0f
RadiantK <pps8853@gmail.com>
2022-07-25 00:27:07 +0900

    스프링 시큐리티 커스텀 테이블 생성
    스프링 시큐리티 커스텀 테이블 생성

--- spring-web05-security/table.sql
+++ spring-web05-security/table.sql
@@ -86,3 +86,21 @@ commit;
 
 select * from users;
 select * from authorities;
+
+
+
+-- 일반적인 회원 테이블 및 권한 테이블
+create table tbl_member (
+    userid varchar2(50) not null primary key,
+    userpw varchar2(100) not null,
+    username varchar2(100) not null,
+    regdate date default sysdate, 
+    updatedate date default sysdate,
+    enabled char(1) default '1'
+);
+
+create table tbl_member_auth (
+    userid varchar2(50) not null,
+    auth varchar2(50) not null,
+    constraint fk_member_auth foreign key (userid) references tbl_member (userid)
+);
