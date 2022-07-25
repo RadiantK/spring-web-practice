73549ace91da3d00673b588d76784d3892a33c99
RadiantK <pps8853@gmail.com>
2022-07-26 00:14:39 +0900

    사용하지 않는 자바 파일 제거
    사용하지 않는 자바 파일 제거

--- spring-web05-security/src/main/java/org/radi/service/SampleService.java
+++
@@ -1,6 +0,0 @@
-package org.radi.service;
-
-public interface SampleService {
-
-	public Integer doAdd(String str1, String str2) throws Exception;
-}
--- spring-web05-security/src/main/java/org/radi/service/SampleTxService.java
+++
@@ -1,6 +0,0 @@
-package org.radi.service;
-
-public interface SampleTxService {
-
-	public void addData(String value);
-}
